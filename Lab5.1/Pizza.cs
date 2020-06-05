35243e55af69f67624b94731727ac8c326b703ed
MTK3852 <mtk835123@gmail.com>
2020-06-05 17:16:32 +0300

    1task

---
+++ Lab5.1/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._1
+{
+    public class Pizza
+    {
+        private string name;
+        private string address;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { name = value; }
+        }
+
+        public Pizza()
+        {
+            this.name = "Rolex";
+            this.address = "вул.Шевченка, 40";
+        }
+    }
+}
